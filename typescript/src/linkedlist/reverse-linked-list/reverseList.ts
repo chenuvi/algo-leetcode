16ad01cb95e5706df068f7811fb3c02f1542be43
chenkun <kun.chen@yintech.cn>
2023-04-23 17:36:34 +0800

    Feat: ts 翻转链表2

--- typescript/src/linkedlist/reverse-linked-list/reverseList.ts
+++ typescript/src/linkedlist/reverse-linked-list/reverseList.ts
@@ -13,7 +13,7 @@
 export function reverseList(head: ListNode | null): ListNode | null {
     let pre: ListNode | null = null
     let cur: ListNode | null = head
-    while (cur) {
+    while (cur !== null) {
         let temp = cur.next
         cur.next = pre
         pre = cur
