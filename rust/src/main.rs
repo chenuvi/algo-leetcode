70dea6debe185a829da1bb1f1d1f01f1776d1777
kunchen16 <kunchen16@iflytek.com>
2025-03-11 20:49:26 +0800

    init(rs): 新建 rust 项目

---
+++ rust/src/main.rs
@@ -0,0 +1,6 @@
+// 使用 crate 路径导入模块
+use algo_leetcode::stack_and_line::*;
+
+fn main() {
+    valid_parentheses::Solution::test();
+}
---
+++ rust/src/stack_and_line/valid_parentheses.rs
@@ -0,0 +1,11 @@
+pub struct Solution {}
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn is_valid(_s: String) -> bool {
+        false
+    }
+    pub fn test() {
+        println!("Hello rust")
+    }
+}
