e6a9b7209f30e78fb2db284707f9fd303f8530b7
kunchen16 <kunchen16@iflytek.com>
2025-04-02 14:46:32 +0800

    feat(rs): 滑动窗口最大值

--- rust/src/stack_and_line/sliding_window_maximum.rs
+++ rust/src/stack_and_line/sliding_window_maximum.rs
@@ -1,5 +1,49 @@
 use crate::Solution;
 
 impl Solution {
-    pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {}
+    pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut queue = MonotonicQueue::new();
+        let mut result = Vec::new();
+        let k = k as usize;
+        // 初始化第一个窗口
+        for i in 0..k {
+            queue.enqueue(*nums.get(i).unwrap());
+        }
+        result.push(queue.top());
+
+        // 滑动窗口
+        for i in k..nums.len() {
+            queue.dequeue(*nums.get(i - k).unwrap());
+            queue.enqueue(*nums.get(i).unwrap());
+            result.push(queue.top());
+        }
+        result
+    }
+}
+
+struct MonotonicQueue {
+    queue: Vec<i32>,
+}
+
+impl MonotonicQueue {
+    pub fn new() -> Self {
+        MonotonicQueue { queue: Vec::new() }
+    }
+
+    pub fn top(&self) -> i32 {
+        *self.queue.first().unwrap()
+    }
+
+    pub fn enqueue(&mut self, value: i32) {
+        while !self.queue.is_empty() && *self.queue.last().unwrap() < value {
+            self.queue.pop();
+        }
+        self.queue.push(value);
+    }
+
+    pub fn dequeue(&mut self, value: i32) {
+        if !self.queue.is_empty() && self.top() == value {
+            self.queue.remove(0);
+        }
+    }
 }
