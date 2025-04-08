b6db17eb6ab6a11314ffb46b23ac69bcd6b2edfd
chenuvi <chenuvi@outlook.com>
2025-04-08 23:00:36 +0800

    feat(rs): top_k_frequent

--- rust/src/stack_and_line/top_k_frequent_elements.rs
+++ rust/src/stack_and_line/top_k_frequent_elements.rs
@@ -1,7 +1,15 @@
 use crate::Solution;
 
 impl Solution {
-    pub fn top_k_frequent(nums: Vec<i32>, _k: i32) -> Vec<i32> {
-        nums
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut count_map = std::collections::HashMap::new();
+        for num in nums {
+            *count_map.entry(num).or_insert(0) += 1; 
+        }
+        let mut count_vec: Vec<(i32, i32)> = count_map.iter().map(|(k, v)| (*k, *v)).collect();
+        count_vec.sort_by(|a, b| b.1.cmp(&a.1));
+        count_vec.iter().map(|(k, _)| *k).take(k as usize).collect();
+        let res =  count_vec.into_iter().map(|(num, _)| num).collect();
+        res
     }
 }
